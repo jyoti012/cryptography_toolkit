04fe42ba471bded35e53ab8af51f13d38bd46dab
jyoti012 <jyoti.suvarna.code@gmail.com>
2023-11-24 10:34:40 +0000

    Try testing with large numbers

--- CS616Toolkit/ElGamalPrivateKeyFinder.java
+++ CS616Toolkit/ElGamalPrivateKeyFinder.java
@@ -5,9 +5,10 @@ import java.math.BigInteger;
 public class ElGamalPrivateKeyFinder {
   public static void main(String[] args) {
     // Given public key (p, g, y)
-    BigInteger p = new BigInteger("3847");
-    BigInteger g = new BigInteger("1892");
-    BigInteger y = new BigInteger("1954");
+    BigInteger p = new BigInteger("99333611752778158313691997689669641978712476822507750499831812831584630785973"); // Prime                                                                                                             // modulus
+    BigInteger g = new BigInteger("89590362417598127889763750591434581694337369135165007793050455118722836261003"); // Generator
+    BigInteger y = new BigInteger("67931852888628538370037206937600742353104147135380482916521197699075937305150"); // Public
+                                                                                                                    // key
 
     // Calculate the private key (x) based on the provided information
     BigInteger x = findPrivateKey(p, g, y);
